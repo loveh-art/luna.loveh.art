04b66ddd7eef2c791c5e45bd9bb2b095c34f4cf8
Luna <imlvnaa@gmail.com>
2023-08-29 09:34:19 -0400

    log

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,11 @@
 import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  // console.log(event.request.url, event.request.headers.get("user-agent"));
+  console.log(
+    `${event.request.headers.get("CF-Connecting-IP")} ${event.request.method} ${
+      event.request.url
+    } ${event.request.headers.get("user-agent")}`
+  );
   if (
     event.request.headers.get("user-agent")?.includes("Discordbot") &&
     !event.url.pathname.startsWith("/discordBot")
