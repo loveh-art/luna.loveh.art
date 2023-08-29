5f3611e1bd8b48ce3f5ef58ff2c6db493c277da4
Luna <imlvnaa@gmail.com>
2023-08-29 09:20:54 -0400

    fix url

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,9 +4,9 @@ export const handle: Handle = async ({ event, resolve }) => {
   console.log(event.request.url, event.request.headers.get("user-agent"));
   if (
     event.request.headers.get("user-agent")?.includes("Discordbot") &&
-    !event.url.pathname.startsWith("/discordBot/")
+    !event.url.pathname.startsWith("/discordBot")
   ) {
-    throw redirect(302, "/discordBot/" + event.url.pathname);
+    throw redirect(302, "/discordBot" + event.url.pathname);
   }
   const response = await resolve(event);
   return response;
