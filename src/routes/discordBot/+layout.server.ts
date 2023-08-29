e9e4e9e795b6756ea16625cef6e61eeac970f17a
Luna <imlvnaa@gmail.com>
2023-08-29 09:07:51 -0400

    fix spotify embed

--- src/routes/discordBot/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-import { getData } from "$lib/server/spotify";
-import type { LayoutServerLoad } from "./$types";
-
-export const load: LayoutServerLoad = async () => {
-  return await getData();
-};
---
+++ src/routes/discordBot/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <meta content={data.song.name} property="og:title" />
+  <meta content="https://lvna.gay/discordBot/embed" property="og:image" />
+  <meta content={data.song.albumUrl} property="og:url" />
+  <meta content={data.song.artist} property="og:description" />
+  <meta content="#ffcb82" property="theme-color" />
+</svelte:head>
