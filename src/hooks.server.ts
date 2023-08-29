f4325e64133735e0318eb3e83f557f9d2542070c
Luna <imlvnaa@gmail.com>
2023-08-29 09:11:12 -0400

    fix pathnames

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { redirect, type Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   if (event.request.headers.get("user-agent")?.includes("Discordbot")) {
-    throw redirect(302, "/discordBot/" + event.request.url.split("/").pop());
+    throw redirect(302, "/discordBot/" + event.url.pathname);
   }
   const response = await resolve(event);
   return response;
